3f3261592d0fac59a332fea315d7d42779de6750
higorsantana-omega <omegadosgames02@gmail.com>
2020-12-05 17:57:38 -0300

    open google

--- bot.py
+++ bot.py
@@ -2,3 +2,6 @@ from selenium import webdriver
 import time
 from webdriver_manager.chrome import ChromeDriverManager
 
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://web.whatsapp.com/')
+time.sleep(10)
