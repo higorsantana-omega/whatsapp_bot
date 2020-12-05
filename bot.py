23ff23767eaaf3f30d3cc11266bf3e0df924f08a
higorsantana-omega <omegadosgames02@gmail.com>
2020-12-05 18:59:32 -0300

    search contact

--- bot.py
+++ bot.py
@@ -5,3 +5,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://web.whatsapp.com/')
 time.sleep(10)
+
+contacts = ['Bot']
+mensagem = 'Oi'
+
+def search_contact(contacts):
+    search_box = driver.find_element_by_xpath('//div[contains(@class, "copyable-text selectable-text")]')
+    time.sleep(5)
+    search_box.click()
+    search_box.send_keys(contacts)
+
+for contact in contacts:
+    search_contact(contacts)
