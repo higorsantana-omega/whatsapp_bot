85b31301b8b06fdb977931fa42bd9f591d1f3e8a
higorsantana-omega <omegadosgames02@gmail.com>
2020-12-08 20:46:07 -0300

    input msg; click and send msg; tamanho msg

--- bot.py
+++ bot.py
@@ -5,25 +5,28 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://web.whatsapp.com/')
-time.sleep(30)
+time.sleep(15)
 
-contacts = ['Bot']
-mensagem = 'Oi'
+mensagem = input('Mensagem a ser enviada: ')
+
+contacts = ['amorzin']
+# mensagem = [].join(reb_msg)
 
 def search_contact(contacts):
     search_box = driver.find_element_by_xpath('//div[contains(@class, "copyable-text selectable-text")]')
-    time.sleep(5)
+    time.sleep(3)
     search_box.click()
     search_box.send_keys(contacts)
     time.sleep(3)
     search_box.send_keys(Keys.ENTER)
 
 def send_msg(mensagem):
-    search_box_msg = driver.find_elements_by_class_name('_1awRl')
-    time.sleep(5)
-    search_box_msg.click()
-    search_box_msg.send_keys(mensagem)
-    search_box_msg.send_keys(Keys.ENTER)
+    search_box_msg = driver.find_elements_by_xpath('//div[contains(@class, "copyable-text selectable-text")]')
+    time.sleep(3)
+    search_box_msg[1].click()
+    for i in range(125):
+        search_box_msg[1].send_keys(mensagem)
+        search_box_msg[1].send_keys(Keys.ENTER)
 
 for contact in contacts:
     search_contact(contacts)
